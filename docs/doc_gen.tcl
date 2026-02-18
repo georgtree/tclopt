aaba396676f8eab4fe81f8d99dc84f2122aca961
georgtree <georgtree@gmail.com>
2026-02-18 23:46:07 +0000

    fixed pygment lexer warning with =

--- docs/doc_gen.tcl
+++ docs/doc_gen.tcl
@@ -31,7 +31,22 @@ if {[llength $argv] == 0 || "html" in $argv} {
 ::fileutil::appendToFile [file join $docDir sphinx conf.py] {html_theme = "classic"
 extensions = [
     "sphinx.ext.githubpages",
-]}
+]
+from pygments.lexers.tcl import TclLexer
+from pygments.token import Operator
+
+class MyTclLexer(TclLexer):
+    def get_tokens_unprocessed(self, text):
+        for i, t, v in super().get_tokens_unprocessed(text):
+            if v == "=":
+                yield i, Operator, v   # or Name.Builtin
+            else:
+                yield i, t, v
+
+def setup(app):
+    from sphinx.highlighting import lexers
+    lexers["tcl"] = MyTclLexer()
+}
 catch {exec sphinx-build -b html [file join $docDir sphinx] [file join $docDir]} errorStr
 puts $errorStr
 
